305c6178652db33dbf0d3d5607bbbc98128a3f1f
Belov Andrey <andrey-belov.06@yandex.ru>
2025-12-02 13:10:02 +0300

    добавлены и сделаны задания среднего уровня

--- lambda, filter, map.py
+++ lambda, filter, map.py
@@ -26,3 +26,25 @@ print('-========-')
 lst=["hello", "world", "python"]
 print(list(map(lambda x:len(x), lst)))
 print('-========-')
+#Cредний уровень
+lst=["hello", "world", "python"]
+lambda_rev=lambda x:x[::-1]
+print(list(map(lambda_rev, lst)))
+print('-========-')
+lst=[2,4,5,6]
+lst2=[2,3,5,3]
+print(list(map(lambda x, y:x**y, lst, lst2)))
+print('-========-')
+lst=[-5, 10, -3, 0, 7, -1, 15]
+print(list(filter(lambda x: True if x>0 and x%5==0 else False, lst)))
+print('-========-')
+lst=["Python", "java", "C++", "JavaScript", "go", "Rust"]
+print(list(filter(lambda x: True if x[0].isupper() and len(x)>3 else False, lst)))
+print('-========-')
+first=[(1, 2), (3, 4), (5, 6)]
+print(list(map(lambda x:x[0]+x[1], first)))
+print('-========-')
+lst=[1,2,3,4,5]
+print(list(map(lambda x: f'число: {x}', lst)))
+print('-========-')
+
