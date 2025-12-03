75514400e73f5c227370b9bf1cb6ab7081f01fc7
Belov Andrey <andrey-belov.06@yandex.ru>
2025-12-03 11:01:49 +0300

    разводим ветки

--- файл для слияния.py
+++ файл для слияния.py
@@ -6,3 +6,11 @@ text='asjdfauewygd'
 vow=filter(lambda x: True if x in vowels else False, text)
 print(list(vow))
 print(list(filter(lambda x: True if x not in vowels else False, text)))
+students = [
+    {"name": "Alice", "age": 20, "grades": [85, 90, 78]},
+    {"name": "Bob", "age": 22, "grades": [70, 65, 80]},
+    {"name": "Charlie", "age": 19, "grades": [95, 88, 92]},
+    {"name": "David", "age": 21, "grades": [60, 72, 68]}
+]
+middle=list(filter(lambda student: True if sum(student['grades'])/len(student['grades']) > 75 else False, students))
+print(middle)
