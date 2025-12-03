8af2971c0f93ef21dca0a7bcf184418b24b094aa
Belov Andrey <andrey-belov.06@yandex.ru>
2025-12-03 10:50:53 +0300

    функция для разветвления

--- файл для слияния.py
+++ файл для слияния.py
@@ -1,3 +1,8 @@
 maximum=lambda lst:  (min(lst), max(lst), sum(lst)/len(lst))
 lst=[1,2,3,4,5,8,-1]
 print(maximum(lst))
+vowels=['a','e','i','o','u',]
+text='asjdfauewygd'
+vow=filter(lambda x: True if x in vowels else False, text)
+print(list(vow))
+print(list(filter(lambda x: True if x not in vowels else False, text)))
